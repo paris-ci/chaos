7b34bff294072b6a7d39ba8a3d7213ca1b844fdd
Arthur <jovart.arthur@gmail.com>
2017-05-23 23:18:36 +0200

    Don't tell me what to do

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ with open(abs_pat_file) as h:
 GITHUB_USER = "chaosbot"
 
 # TEST SETTING PLEASE IGNORE
-TEST = False 
+TEST = True 
 
 # the amount of time chaosbot should sleep between polling for ready prs
 SLEEP_TIME = 30
